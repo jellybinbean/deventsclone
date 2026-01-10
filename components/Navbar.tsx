bfcfb2c7d1ca94f2e0ca06a70bff79f37614c15e
jellybinbean <124924713+jellybinbean@users.noreply.github.com>
2026-01-10 11:33:23 +0300

    implement posthog

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import Image from "next/image"
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <Link href="/"
+            className="logo">
+                <Image
+                
+                src="/icons/logo.png"
+                alt="logo"
+                width={24}
+                height={24}
+                />  
+                <p>Devents</p>             
+            </Link>
+            <ul>
+                <Link href="/">Home</Link>
+                <Link href="/">Events</Link>
+                <Link href="/">Create Events</Link>
+         
+            </ul>         
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
